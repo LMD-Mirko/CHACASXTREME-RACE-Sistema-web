0c795af5a9e6b950c84bb31406773673ac9d742d
LMD-Mirko <mirkodanielramoslimas20@gmail.com>
2026-07-10 16:51:46 -0500

    chore: configure API proxy rewrites in vercel.json and update axios base URL

--- src/core/network/axios.js
+++ src/core/network/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://24.199.82.193:8888/',
+  baseURL: '/',
   timeout: 10000,
   headers: {
     'Accept': 'application/json',
